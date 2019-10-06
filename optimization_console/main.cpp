9d0b527a7725525e2f5da60638dc70646fa889a2
Kapatsa <31724594+Kapatsa@users.noreply.github.com>
2019-10-06 13:02:42 +0300

    Initial Commit

---
+++ optimization_console/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  optimization_console
+//
+//  Created by David Kapatsa on 06.10.2019.
+//  Copyright © 2019 David Kapatsa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
